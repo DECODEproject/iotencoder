173af8a148bafcd2ff478cbcd294d375ca8adc14
Sam Mulube <sam@thingful.net>
2018-07-16 16:26:55 +0100

    First pass at implementing binning of data, with tests

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -2,38 +2,32 @@ package pipeline
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"time"
 
-	"github.com/pkg/errors"
-	"github.com/thingful/zenroom-go"
+	"github.com/guregu/null"
 
 	kitlog "github.com/go-kit/kit/log"
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/thingful/iotencoder/pkg/postgres"
+	datastore "github.com/thingful/twirp-datastore-go"
+	"github.com/thingful/zenroom-go"
 
 	"github.com/thingful/iotencoder/pkg/lua"
-
-	datastore "github.com/thingful/twirp-datastore-go"
+	"github.com/thingful/iotencoder/pkg/postgres"
 )
 
 var (
-	// datastoreErrorCounter is a prometheus counter recording a count of any
-	// errors that occur when writing to the datastore
-	datastoreErrorCounter = prometheus.NewCounter(
+	// pipelineErrorCounter is a counter vec used to count any errors that happen
+	// while processing incoming payloads.
+	pipelineErrorCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "decode_datastore_errors",
-			Help: "Count of errors writing to datastore",
+			Name: "decode_pipeline_errors",
+			Help: "Count of errors while processing the pipeline",
 		},
-	)
-
-	// zenroomErrorCounter is a prometheus counter recording a count of any errors
-	// that occur when invoking zenroom.
-	zenroomErrorCounter = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Name: "decode_zenroom_errors",
-			Help: "Count of errors invoking zenroom",
-		},
+		[]string{"cause"},
 	)
 
 	// datastoreWriteHistogram is a prometheus histogram recording successful
@@ -56,9 +50,8 @@ var (
 )
 
 func init() {
-	prometheus.MustRegister(datastoreErrorCounter)
+	prometheus.MustRegister(pipelineErrorCounter)
 	prometheus.MustRegister(datastoreWriteHistogram)
-	prometheus.MustRegister(zenroomErrorCounter)
 	prometheus.MustRegister(zenroomHistogram)
 }
 
@@ -80,63 +73,109 @@ type processor struct {
 	datastore datastore.Datastore
 	logger    kitlog.Logger
 	verbose   bool
+	sensors   map[int]SensorInfo
 }
 
 // NewProcessor is a constructor function that takes as input an instantiated
 // datastore client, and a logger. It returns the instantiated processor which
 // is ready for use. Note we pass in the datastore instance so that we can
 // supply a mock for testing.
-func NewProcessor(ds datastore.Datastore, verbose bool, logger kitlog.Logger) Processor {
+func NewProcessor(ds datastore.Datastore, verbose bool, logger kitlog.Logger) (Processor, error) {
 	logger = kitlog.With(logger, "module", "pipeline")
+
+	// load sensor data from json asset
+	sensorData, err := Asset("sensors.json")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load sensors.json")
+	}
+
+	// unmarshal the json
+	var sensorInfo []SensorInfo
+	err = json.Unmarshal(sensorData, &sensorInfo)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal sensors.json")
+	}
+
+	// and create map keyed by sensor ID
+	sensors := make(map[int]SensorInfo)
+	for _, sensor := range sensorInfo {
+		sensors[sensor.ID] = sensor
+	}
 
 	return &processor{
 		datastore: ds,
 		logger:    logger,
 		verbose:   verbose,
-	}
+		sensors:   sensors,
+	}, nil
 }
 
 // Process is the function that actually does the work of dispatching the
 // received data to all destination streams after applying whatever processing
 // the stream specifies. Currently we do the simplest thing of just writing the
 // data directly to the datastore.
-func (p *processor) Process(device *postgres.Device, payload []byte) error {
+func (p *processor) Process(device *postgres.Device, data []byte) error {
 
 	// check payload
-	if payload == nil || len(payload) == 0 {
+	if data == nil || len(data) == 0 {
+		pipelineErrorCounter.With(prometheus.Labels{"cause": "emptyBody"}).Inc()
 		return errors.New("empty payload received")
+	}
+
+	var rawPayload RawPayload
+	err := json.Unmarshal(data, &rawPayload)
+	if err != nil {
+		pipelineErrorCounter.With(prometheus.Labels{"cause": "unmarshalPayload"}).Inc()
+		return errors.Wrap(err, "failed to unmarshal incoming json")
+	}
+
+	if len(rawPayload.Data) != 1 {
+		return fmt.Errorf("Unexpected data length, expected: 1, got: %v\n", len(rawPayload.Data))
 	}
 
 	// read script from go-bindata asset
 	script, err := lua.Asset("encrypt.lua")
 	if err != nil {
+		pipelineErrorCounter.With(prometheus.Labels{"cause": "zenroomScript"}).Inc()
 		return errors.Wrap(err, "failed to read zenroom script")
 	}
 
 	for _, stream := range device.Streams {
-		if p.verbose {
-			p.logger.Log("public_key", stream.PublicKey, "user_uid", device.UserUID, "msg", "writing data")
-		}
-
 		start := time.Now()
 
+		payload, err := Process(&rawPayload, device, stream, p.sensors)
+		if err != nil {
+			pipelineErrorCounter.With(prometheus.Labels{"cause": "processing"}).Inc()
+			p.logger.Log("err", err, "msg", "failed to process payload")
+			continue
+		}
+
+		// note we have to base64 encode our payload as it may have non-ASCII
+		// characters after metadata processing.
 		encodedPayload, err := zenroom.Exec(
 			script,
-			[]byte(fmt.Sprintf(`{"public": "%s", "private": "%s" }`, stream.PublicKey, device.PrivateKey)),
-			payload,
+			[]byte(fmt.Sprintf(`{"public":"%s","private":"%s"}`, stream.PublicKey, device.PrivateKey)),
+			[]byte(base64.StdEncoding.EncodeToString(payload)),
 		)
 
 		duration := time.Since(start)
 
 		if err != nil {
-			zenroomErrorCounter.Inc()
-			return err
+			pipelineErrorCounter.With(prometheus.Labels{"cause": "zenroom"}).Inc()
+			p.logger.Log("err", err, "msg", "failed to encrypt payload")
+			continue
 		}
 
 		zenroomHistogram.Observe(duration.Seconds())
 
 		if p.verbose {
-			p.logger.Log("encodedPayload", string(encodedPayload))
+			p.logger.Log(
+				"public_key", stream.PublicKey,
+				"user_uid", device.UserUID,
+				"processedPayload", string(payload),
+				"encodedPayload", string(encodedPayload),
+				"msg", "writing data",
+			)
 		}
 
 		start = time.Now()
@@ -150,13 +189,148 @@ func (p *processor) Process(device *postgres.Device, payload []byte) error {
 		duration = time.Since(start)
 
 		if err != nil {
-			datastoreErrorCounter.Inc()
-
-			return err
+			pipelineErrorCounter.With(prometheus.Labels{"cause": "datastore"}).Inc()
+			p.logger.Log("err", err, "msg", "failed to write to datstore")
+			continue
 		}
 
 		datastoreWriteHistogram.Observe(duration.Seconds())
 	}
 
 	return nil
+}
+
+// Process takes as input a RawPayload instance, as well as the current Device
+// and Stream. The passed in Device contains the device metadata, and the Stream
+// defines a list of operations to apply to the received data
+func Process(rawPayload *RawPayload, device *postgres.Device, stream *postgres.Stream, sensors map[int]SensorInfo) ([]byte, error) {
+	data := rawPayload.Data[0]
+
+	payload := Payload{
+		Location: Location{
+			Longitude: device.Longitude,
+			Latitude:  device.Latitude,
+			Exposure:  device.Exposure,
+		},
+		RecordedAt: data.RecordedAt,
+		Sensors:    []Sensor{},
+	}
+
+	if len(stream.Entitlements) == 0 {
+		// add all channels to the output
+		for _, rawSensor := range data.Sensors {
+			if sensorInfo, ok := sensors[rawSensor.ID]; ok {
+				sensor := Sensor{
+					ID:          rawSensor.ID,
+					Name:        sensorInfo.Name,
+					Description: sensorInfo.Description,
+					Unit:        sensorInfo.Unit,
+					Type:        Share,
+					Value:       null.FloatFrom(rawSensor.Value),
+				}
+
+				payload.Sensors = append(payload.Sensors, sensor)
+			}
+		}
+	} else {
+		// iterate over entitlements
+		for _, entitlement := range stream.Entitlements {
+			if sensorInfo, ok := sensors[entitlement.SensorID]; ok {
+				rawSensor, err := findSensor(entitlement.SensorID, data.Sensors)
+				if err != nil {
+					return nil, err
+				}
+
+				sensor, err := makeProcessedSensor(&entitlement, &sensorInfo, rawSensor)
+				if err != nil {
+					return nil, errors.Wrap(err, "failed to generate processed sensor")
+				}
+
+				payload.Sensors = append(payload.Sensors, *sensor)
+			}
+		}
+	}
+
+	// add specified channels to the output
+
+	return json.Marshal(payload)
+}
+
+func makeProcessedSensor(entitlement *postgres.Entitlement, sensorInfo *SensorInfo, rawSensor *RawSensor) (*Sensor, error) {
+	sensor := Sensor{
+		ID:          entitlement.SensorID,
+		Name:        sensorInfo.Name,
+		Description: sensorInfo.Description,
+		Unit:        sensorInfo.Unit,
+	}
+
+	switch entitlement.Action {
+	case string(Share):
+		sensor.Type = Share
+		sensor.Value = null.FloatFrom(rawSensor.Value)
+	case string(Bin):
+		sensor.Type = Bin
+		sensor.Bins = entitlement.Bins
+		sensor.Values = BinnedValues(rawSensor.Value, entitlement.Bins)
+	case string(MovingAvg):
+		sensor.Type = MovingAvg
+		sensor.Interval = entitlement.Interval
+	default:
+		return nil, fmt.Errorf("Unknown entitlement action: %s", entitlement.Action)
+	}
+
+	return &sensor, nil
+}
+
+func findSensor(sensorID int, rawSensors []RawSensor) (*RawSensor, error) {
+	for _, rawSensor := range rawSensors {
+		if sensorID == rawSensor.ID {
+			return &rawSensor, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Unable to find sensor with id: %v", sensorID)
+}
+
+// BinnedValues returns a slice containing the binned output for the given
+// parameters. We pass in the received value as well as a slice which defines
+// the bin boundaries. We define the boundaries slice where each value
+// represents the upper inclusive boundary of a bin. There is an implicit +Inf
+// bound after the last value.
+func BinnedValues(value float64, bins []float64) []int {
+	// example
+	// value: 67.212
+	// bins: [40,80]
+	// expected ouput: [0,1,0]
+
+	binnedVals := make([]int, len(bins)+1)
+
+	for i, _ := range binnedVals {
+		switch {
+		case i == 0:
+			if value < bins[i] {
+				binnedVals[i] = 1
+				continue
+			}
+			binnedVals[i] = 0
+		case i == len(binnedVals)-1:
+			if value >= bins[i-1] {
+				binnedVals[i] = 1
+				continue
+			}
+			binnedVals[i] = 0
+		default:
+			if value >= bins[i-1] && value < bins[i] {
+				binnedVals[i] = 1
+				continue
+			}
+			binnedVals[i] = 0
+		}
+	}
+
+	//if value >= bins[len(bins)-1] {
+	//binnedVals[len(binnedVals)-1] = 1
+	//}
+
+	return binnedVals
 }
--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -16,60 +16,118 @@ import (
 
 func TestProcess(t *testing.T) {
 	logger := kitlog.NewNopLogger()
-	ds := mocks.Datastore{}
+	payload := []byte(`{"data":[{"recorded_at":"2018-06-03T22:26:02Z","sensors":[{"id":10,"value":100},{"id":29,"value":67.02939},{"id":12,"value":22.41268}]}]}`)
 
-	payload := []byte("my-data")
-	encodedPayload := []byte(`73z7QQWCd4y6bOKZHW7NeA==`)
-
-	// set up two mock responses
-	ds.On(
-		"WriteData",
-		context.Background(),
-		&datastore.WriteRequest{
-			PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
-			UserUid:   "bob",
-			Data:      encodedPayload,
-		},
-	).Return(
-		&datastore.WriteResponse{},
-		nil,
-	)
-
-	ds.On(
-		"WriteData",
-		context.Background(),
-		&datastore.WriteRequest{
-			PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
-			UserUid:   "bob",
-			Data:      encodedPayload,
-		},
-	).Return(
-		&datastore.WriteResponse{},
-		nil,
-	)
-
-	processor := pipeline.NewProcessor(datastore.Datastore(&ds), true, logger)
-
-	device := &postgres.Device{
-		UserUID:    "bob",
-		PrivateKey: `Dmchv5dXlMgST8ZdXOaEHmT+HvSj44nvgd0PsiSL\/FE=`,
-		Streams: []*postgres.Stream{
-			{
-				PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
+	testcases := []struct {
+		label             string
+		device            *postgres.Device
+		encryptedPayloads [][]byte
+	}{
+		{
+			label: "no entitlements",
+			device: &postgres.Device{
+				UserUID:    "bob",
+				PrivateKey: `Dmchv5dXlMgST8ZdXOaEHmT+HvSj44nvgd0PsiSL\/FE=`,
+				Longitude:  2.5,
+				Latitude:   2.5,
+				Exposure:   "INDOOR",
+				Streams: []*postgres.Stream{
+					{
+						PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
+					},
+				},
 			},
-			{
-				PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
+			encryptedPayloads: [][]byte{
+				[]byte(`B5yRzPqlmh+4rmSnxNMJ1W8NBv8Om0TkvWraCx27u1H+yRL+P+DTZ6+YjoSKzvHeWEOStuJt7XnFY8H2QiTiLwjkmRoxBvzqCZ2ffbz7zAU7di8PzrRtRF/uRx0LgLrtzbfbJTgIiRvCr0D3x+nJIHIsI9TzEI0rAqT6uNcNnkNBmypcsFssRbwg1yRg4FIl53iIQ3A8c23B74NYy+24PRHidE+ncW7T2FxjhC8a4PB8UEaDcB0cmT4RJM+OX2hrzCBizv8mktAwbJKH5aHai92SybqV49sIFO48xJqnlINNHZh/pZZURFrS1lp4bKEaKOuh5G0mkxtvgADkhB0BVCKwnaFNpiy4NYddGqQZRcam7CyIklF81obOyWAgc+abxfDIjjdt6GVDznQGpIRr3QMnx4E+qfvsc2R/+DunG+ECrKo1FwU+MXHV6N3Foql9zRjijp/uXJiP6Vsiy3GgZgBC8ZzsgKVxYAM08WEyFD0gT+p/uqBhqzBI8EXsaxdQgEHmhvenBX7er6EJ3r30Ye9tau/4X/vMLYGLvim4hTA=`),
+			},
+		},
+		{
+			label: "share entitlement",
+			device: &postgres.Device{
+				UserUID:    "bob",
+				PrivateKey: `Dmchv5dXlMgST8ZdXOaEHmT+HvSj44nvgd0PsiSL\/FE=`,
+				Longitude:  2.5,
+				Latitude:   2.5,
+				Exposure:   "INDOOR",
+				Streams: []*postgres.Stream{
+					&postgres.Stream{
+						PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
+						Entitlements: []postgres.Entitlement{
+							postgres.Entitlement{
+								SensorID: 29,
+								Action:   string(pipeline.Share),
+							},
+						},
+					},
+				},
+			},
+			encryptedPayloads: [][]byte{
+				[]byte(`B5yRzPqlmh+4rmSnxNMJ1W8NBv8Om0TkvWraCx27u1H+yRL+P+DTZ6+YjoSKzvHeWEOStuJt7XnFY8H2QiTiLwjkmRoxBvzqCZ2ffbz7zAU7di8PzrRtRF/uRx0LgLrtzbfbJTgIiRvCr0D3x+nJIHIsI9TzEI0rAqT6uNcNnkNBmypcsFssRbwg1yRg4FIl53iIQ3A8c23B74NYy+24PRHidE+ncW7T2FxjhC8a4PB8UEaDcB0cmT4RJM+OX2hrzCBizv8mktAwbJKH5aHai92SybqV49sIFO48xJqnlINNHZh/pZZURFrS1lp4bKEaKOuh5G0mkxtvgADkhB0BVCKwnaFNpiy4NYddGqQZRcabYXoO+slBgMAOJZbU7tU6`),
+			},
+		},
+		{
+			label: "binning entitlement",
+			device: &postgres.Device{
+				UserUID:    "bob",
+				PrivateKey: `Dmchv5dXlMgST8ZdXOaEHmT+HvSj44nvgd0PsiSL\/FE=`,
+				Longitude:  2.5,
+				Latitude:   2.5,
+				Exposure:   "INDOOR",
+				Streams: []*postgres.Stream{
+					&postgres.Stream{
+						PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
+						Entitlements: []postgres.Entitlement{
+							postgres.Entitlement{
+								SensorID: 29,
+								Action:   string(pipeline.Bin),
+								Bins:     []float64{40, 80},
+							},
+						},
+					},
+				},
+			},
+			encryptedPayloads: [][]byte{
+				[]byte(`B5yRzPqlmh+4rmSnxNMJ1W8NBv8Om0TkvWraCx27u1H+yRL+P+DTZ6+YjoSKzvHeWEOStuJt7XnFY8H2QiTiLwjkmRoxBvzqCZ2ffbz7zAU7di8PzrRtRF/uRx0LgLrtzbfbJTgIiRvCr0D3x+nJIHIsI9TzEI0rAqT6uNcNnkNBmypcsFssRbwg1yRg4FIl53iIQ3A8c23B74NYy+24PRHidE+ncW7T2FxjhC8a4PB8UEaDcB0cmT4RJM+OX2hrzCBizv8mktAwbJKH5aHai92SybqV49sIFO48xJqnlINNHZh/pZZURFrS1lp4bKEa74SwY/CQCkaKeRbl3k5lcK8dr5bT+MNeLhjyZjOhcOrvyq4ZpXXFALtzcRqdlgS79zMS6I9Wboh37Vy6Y5G9T2yOIaaDNUfFXBnhiiLwugk=`),
 			},
 		},
 	}
 
-	err := processor.Process(device, payload)
-	assert.Nil(t, err)
+	for _, tc := range testcases {
+		t.Run(tc.label, func(t *testing.T) {
+			ds := mocks.Datastore{}
 
-	ds.AssertExpectations(t)
+			// set up mock responses
+			for _, encryptedPayload := range tc.encryptedPayloads {
+				ds.On(
+					"WriteData",
+					context.Background(),
+					&datastore.WriteRequest{
+						PublicKey: `BCPcL\/paSqcvCgh2Uu6LbN7o67BFY0cnqfkNUiza62fMQzDFC\/zgB5eSA\/h6nXpuSpUIg7JSOBMoxkavrL5FOj4=`,
+						UserUid:   "bob",
+						Data:      encryptedPayload,
+					},
+				).Return(
+					&datastore.WriteResponse{},
+					nil,
+				)
+			}
+
+			// create new processor
+			processor, err := pipeline.NewProcessor(datastore.Datastore(&ds), true, logger)
+			assert.Nil(t, err)
+
+			// run the processor
+			err = processor.Process(tc.device, payload)
+			assert.Nil(t, err)
+
+			// and assert on expectations
+			ds.AssertExpectations(t)
+		})
+	}
 }
 
 func TestProcessWithError(t *testing.T) {
+	t.Skip()
 	logger := kitlog.NewNopLogger()
 	ds := mocks.Datastore{}
 
@@ -89,7 +147,9 @@ func TestProcessWithError(t *testing.T) {
 		errors.New("error"),
 	)
 
-	processor := pipeline.NewProcessor(datastore.Datastore(&ds), true, logger)
+	processor, err := pipeline.NewProcessor(datastore.Datastore(&ds), true, logger)
+	assert.Nil(t, err)
+
 	device := &postgres.Device{
 		UserUID:    "bob",
 		PrivateKey: `Dmchv5dXlMgST8ZdXOaEHmT+HvSj44nvgd0PsiSL\/FE=`,
@@ -100,9 +160,74 @@ func TestProcessWithError(t *testing.T) {
 		},
 	}
 
-	err := processor.Process(device, payload)
+	err = processor.Process(device, payload)
 	assert.NotNil(t, err)
 	assert.Equal(t, "error", err.Error())
 
 	ds.AssertExpectations(t)
+}
+
+func TestBinnedValues(t *testing.T) {
+	testcases := []struct {
+		label    string
+		value    float64
+		bins     []float64
+		expected []int
+	}{
+		{
+			label:    "single bucket, first",
+			value:    39.2,
+			bins:     []float64{40},
+			expected: []int{1, 0},
+		},
+		{
+			label:    "single bucket, second",
+			value:    67.12,
+			bins:     []float64{40},
+			expected: []int{0, 1},
+		},
+		{
+			label:    "two buckets, first",
+			value:    39.2,
+			bins:     []float64{40, 60},
+			expected: []int{1, 0, 0},
+		},
+		{
+			label:    "two buckets, middle",
+			value:    47.24,
+			bins:     []float64{40, 60},
+			expected: []int{0, 1, 0},
+		},
+		{
+			label:    "two buckets, above",
+			value:    67.24,
+			bins:     []float64{40, 60},
+			expected: []int{0, 0, 1},
+		},
+		{
+			label:    "two buckets, on lower boundary",
+			value:    40,
+			bins:     []float64{40, 60},
+			expected: []int{0, 1, 0},
+		},
+		{
+			label:    "two buckets, on upper boundary",
+			value:    60,
+			bins:     []float64{40, 60},
+			expected: []int{0, 0, 1},
+		},
+		{
+			label:    "three buckets",
+			value:    43.15,
+			bins:     []float64{40, 60, 80},
+			expected: []int{0, 1, 0, 0},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.label, func(t *testing.T) {
+			got := pipeline.BinnedValues(tc.value, tc.bins)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
 }
