704381c5a9d326b09b02c3b65ea1ab8c7876104e
Sam Mulube <sam@umbrellium.co.uk>
2018-12-12 12:13:40 +0000

    Fix pipeline tests to include redis mock

--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -22,8 +22,6 @@ func TestProcess(t *testing.T) {
 	logger := kitlog.NewNopLogger()
 	ds := mocks.Datastore{}
 
-	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
-
 	// set up a mock response
 	ds.On(
 		"WriteData",
@@ -34,7 +32,21 @@ func TestProcess(t *testing.T) {
 		nil,
 	)
 
-	processor := pipeline.NewProcessor(datastore.Datastore(&ds), true, logger)
+	rd := mocks.Redis{}
+	rd.On(
+		"MovingAverage",
+		12.58,
+		"foo",
+		12,
+		uint32(900),
+	).Return(
+		12.58,
+		nil,
+	)
+
+	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
+
+	processor := pipeline.NewProcessor(datastore.Datastore(&ds), &rd, true, logger)
 
 	device := &postgres.Device{
 		DeviceToken: "foo",
@@ -53,7 +65,8 @@ func TestProcess(t *testing.T) {
 					},
 					&postgres.Operation{
 						SensorID: 12,
-						Action:   postgres.Share,
+						Action:   postgres.MovingAverage,
+						Interval: 900,
 					},
 					&postgres.Operation{
 						SensorID: 29,
@@ -69,13 +82,12 @@ func TestProcess(t *testing.T) {
 	assert.Nil(t, err)
 
 	ds.AssertExpectations(t)
+	rd.AssertExpectations(t)
 }
 
-func TestProcessWithError(t *testing.T) {
+func TestProcessWithDatastoreError(t *testing.T) {
 	logger := kitlog.NewNopLogger()
 	ds := mocks.Datastore{}
-
-	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
 
 	ds.On(
 		"WriteData",
@@ -86,7 +98,21 @@ func TestProcessWithError(t *testing.T) {
 		errors.New("error"),
 	)
 
-	processor := pipeline.NewProcessor(datastore.Datastore(&ds), true, logger)
+	rd := mocks.Redis{}
+	rd.On(
+		"MovingAverage",
+		12.58,
+		"foo",
+		12,
+		uint32(900),
+	).Return(
+		12.58,
+		nil,
+	)
+
+	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
+
+	processor := pipeline.NewProcessor(&ds, &rd, true, logger)
 	device := &postgres.Device{
 		DeviceToken: "foo",
 		Streams: []*postgres.Stream{
@@ -102,4 +128,69 @@ func TestProcessWithError(t *testing.T) {
 	assert.Equal(t, "error", err.Error())
 
 	ds.AssertExpectations(t)
+}
+
+func TestProcessWithRedisError(t *testing.T) {
+	logger := kitlog.NewNopLogger()
+	ds := mocks.Datastore{}
+
+	ds.On(
+		"WriteData",
+		context.Background(),
+		mock.Anything,
+	).Return(
+		&datastore.WriteResponse{},
+		nil,
+	)
+
+	rd := mocks.Redis{}
+	rd.On(
+		"MovingAverage",
+		12.58,
+		"foo",
+		12,
+		uint32(900),
+	).Return(
+		0.0,
+		errors.New("error"),
+	)
+
+	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
+
+	processor := pipeline.NewProcessor(&ds, &rd, true, logger)
+	device := &postgres.Device{
+		DeviceToken: "foo",
+		Streams: []*postgres.Stream{
+			{
+				PolicyID:  "smartcitizen",
+				PublicKey: `BBLewg4VqLR38b38daE7Fj\/uhr543uGrEpyoPFgmFZK6EZ9g2XdK\/i65RrSJ6sJ96aXD3DJHY3Me2GJQO9\/ifjE=`,
+				Operations: postgres.Operations{
+					&postgres.Operation{
+						SensorID: 13,
+						Action:   postgres.Share,
+					},
+					&postgres.Operation{
+						SensorID: 14,
+						Action:   postgres.Share,
+					},
+					&postgres.Operation{
+						SensorID: 12,
+						Action:   postgres.MovingAverage,
+						Interval: 900,
+					},
+					&postgres.Operation{
+						SensorID: 29,
+						Action:   postgres.Bin,
+						Bins:     []float64{30, 80, 120},
+					},
+				},
+			},
+		},
+	}
+
+	err := processor.Process(device, payload)
+	assert.NotNil(t, err)
+	assert.Equal(t, "failed to calculate moving average: error", err.Error())
+
+	//ds.AssertExpectations(t)
 }
