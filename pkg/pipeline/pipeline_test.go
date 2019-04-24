e79e186fa1a5e02ddffaf441378bf0a57f78cef2
Sam Mulube <sam@umbrellium.co.uk>
2019-04-24 02:14:22 +0100

    Fix tests

--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -21,6 +21,7 @@ import (
 )
 
 func decryptData(t *testing.T, call mock.Call, secKey string) (*smartcitizen.Device, error) {
+	t.Helper()
 	req := call.Arguments[1].(*datastore.WriteRequest)
 
 	decryptKeys := []byte(fmt.Sprintf(`{"community_seckey":"%s"}`, secKey))
@@ -62,8 +63,8 @@ func TestProcess(t *testing.T) {
 		nil,
 	)
 
-	rd := mocks.Redis{}
-	rd.On(
+	mv := mocks.MovingAverager{}
+	mv.On(
 		"MovingAverage",
 		12.58,
 		"foo",
@@ -76,7 +77,7 @@ func TestProcess(t *testing.T) {
 
 	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
 
-	processor := pipeline.NewProcessor(datastore.Datastore(&ds), &rd, true, logger)
+	processor := pipeline.NewProcessor(datastore.Datastore(&ds), &mv, true, logger)
 
 	device := &postgres.Device{
 		DeviceToken: "foo",
@@ -112,13 +113,14 @@ func TestProcess(t *testing.T) {
 	assert.Nil(t, err)
 
 	ds.AssertExpectations(t)
-	rd.AssertExpectations(t)
+	mv.AssertExpectations(t)
 
 	assert.Len(t, ds.Calls, 1)
 
 	decryptedDevice, err := decryptData(t, ds.Calls[0], "D19GsDTGjLBX23J281SNpXWUdu+oL6hdAJ0Zh6IrRHA=")
 	assert.Nil(t, err)
 
+	assert.NotNil(t, decryptedDevice)
 	assert.Len(t, decryptedDevice.Sensors, 4)
 }
 
@@ -136,11 +138,11 @@ func TestProcessWithNoOperations(t *testing.T) {
 		nil,
 	)
 
-	rd := mocks.Redis{}
+	mv := mocks.MovingAverager{}
 
 	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
 
-	processor := pipeline.NewProcessor(datastore.Datastore(&ds), &rd, true, logger)
+	processor := pipeline.NewProcessor(datastore.Datastore(&ds), &mv, true, logger)
 
 	device := &postgres.Device{
 		DeviceToken: "foo",
@@ -157,7 +159,7 @@ func TestProcessWithNoOperations(t *testing.T) {
 	assert.Nil(t, err)
 
 	ds.AssertExpectations(t)
-	rd.AssertExpectations(t)
+	mv.AssertExpectations(t)
 
 	assert.Len(t, ds.Calls, 1)
 
@@ -180,21 +182,11 @@ func TestProcessWithDatastoreError(t *testing.T) {
 		errors.New("error"),
 	)
 
-	rd := mocks.Redis{}
-	rd.On(
-		"MovingAverage",
-		12.58,
-		"foo",
-		12,
-		uint32(900),
-	).Return(
-		12.58,
-		nil,
-	)
+	mv := mocks.MovingAverager{}
 
 	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
 
-	processor := pipeline.NewProcessor(&ds, &rd, true, logger)
+	processor := pipeline.NewProcessor(&ds, &mv, true, logger)
 	device := &postgres.Device{
 		DeviceToken: "foo",
 		Streams: []*postgres.Stream{
@@ -210,69 +202,4 @@ func TestProcessWithDatastoreError(t *testing.T) {
 	assert.Equal(t, "error", err.Error())
 
 	ds.AssertExpectations(t)
-}
-
-func TestProcessWithRedisError(t *testing.T) {
-	logger := kitlog.NewNopLogger()
-	ds := mocks.Datastore{}
-
-	ds.On(
-		"WriteData",
-		context.Background(),
-		mock.Anything,
-	).Return(
-		&datastore.WriteResponse{},
-		nil,
-	)
-
-	rd := mocks.Redis{}
-	rd.On(
-		"MovingAverage",
-		12.58,
-		"foo",
-		12,
-		uint32(900),
-	).Return(
-		0.0,
-		errors.New("error"),
-	)
-
-	payload := []byte(`{"data":[{"recorded_at":"2018-12-11T14:46:44Z","sensors":[{"id":13, "value":51.00},{"id":14, "value":426.42},{"id":12, "value":12.58},{"id":29, "value":79.35},{"id":53, "value":51.00},{"id":58, "value":101.56},{"id":89, "value":4.00},{"id":87, "value":7.00},{"id":88, "value":7.00}]}]}`)
-
-	processor := pipeline.NewProcessor(&ds, &rd, true, logger)
-	device := &postgres.Device{
-		DeviceToken: "foo",
-		Streams: []*postgres.Stream{
-			{
-				CommunityID: "smartcitizen",
-				PublicKey:   `BBLewg4VqLR38b38daE7Fj\/uhr543uGrEpyoPFgmFZK6EZ9g2XdK\/i65RrSJ6sJ96aXD3DJHY3Me2GJQO9\/ifjE=`,
-				Operations: postgres.Operations{
-					&postgres.Operation{
-						SensorID: 13,
-						Action:   postgres.Share,
-					},
-					&postgres.Operation{
-						SensorID: 14,
-						Action:   postgres.Share,
-					},
-					&postgres.Operation{
-						SensorID: 12,
-						Action:   postgres.MovingAverage,
-						Interval: 900,
-					},
-					&postgres.Operation{
-						SensorID: 29,
-						Action:   postgres.Bin,
-						Bins:     []float64{30, 80, 120},
-					},
-				},
-			},
-		},
-	}
-
-	err := processor.Process(device, payload)
-	assert.NotNil(t, err)
-	assert.Equal(t, "failed to calculate moving average: error", err.Error())
-
-	//ds.AssertExpectations(t)
 }
